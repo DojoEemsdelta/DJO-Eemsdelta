523007a2a2764247d6a86303d4e73c79f4b41356
DojoEemsdelta <djo.app.eems@gmail.com>
2023-01-16 17:47:30 +0100

    express

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const http = require('http');
+const express = require('express');
 
-// Create an instance of the http server to handle HTTP requests
-let app = http.createServer((req, res) => {
-    // Set a response type of plain text for the response
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+// Create Express app
+const app = express();
 
-    // Send back a response and end the connection
-    res.end('Hello World!\n');
-});
+// A sample route
+app.get('/', (req, res) => res.send('Hello World'));
 
-// Start the server on port 3000
-app.listen(3000, '127.0.0.1');
-console.log('Node server running on port 3000');
+// Start the Express server
+app.listen(3000, () => console.log('Server running on port 3000'));
